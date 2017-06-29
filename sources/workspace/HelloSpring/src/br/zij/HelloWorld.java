4aa066a1c72ad4faf324c10308311140213adbab
zijonas <jonas.ziel@terra.com.br>
2017-06-29 16:05:06 -0300

    New examples on tutorial

--- sources/workspace/HelloSpring/src/br/zij/HelloWorld.java
+++ sources/workspace/HelloSpring/src/br/zij/HelloWorld.java
@@ -1,14 +1,18 @@
 package br.zij;
 
 public class HelloWorld {
-		private String message;
+	private String message;
 
-		public String getMessage() {
-			return "The message is: " + message;
-		}
+	public String getMessage() {
+		return "The message is: " + message;
+	}
 
-		public void setMessage(String message) {
-			this.message = message;
-		}
-		
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void init() {
+		this.message = "Is initialized";
+	}
+
 }
--- sources/workspace/HelloSpring/src/br/zij/MainApp.java
+++ sources/workspace/HelloSpring/src/br/zij/MainApp.java
@@ -6,9 +6,24 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 
 	public static void main(String[] args) {
+//		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+//		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+//		System.out.println(obj.getMessage());
+//		obj.setMessage("Say Hue");
+//		System.out.println(obj.getMessage());
+		
+		
+//		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+//		TextEditor txte = (TextEditor) context.getBean("textEditor");
+//		txte.spellCheck();
+		
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-		System.out.println(obj.getMessage());
+		JavaCollection jc = (JavaCollection) context.getBean("javaCollections");
+		jc.getMyList();
+		jc.getMyMap();
+		jc.getMyProperties();
+		jc.getMySet();
+		
 	}
 
 }
