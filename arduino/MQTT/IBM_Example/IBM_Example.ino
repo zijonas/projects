d9f0d79e69e191adbebb3ba49dfebb7dc7ab46f7
zijonas <jonas.ziel@terra.com.br>
2017-08-21 22:52:29 -0300

    Teste an mqtt commited

--- arduino/MQTT/IBM_Example/IBM_Example.ino
+++ arduino/MQTT/IBM_Example/IBM_Example.ino
@@ -1,33 +1,35 @@
 /*
- MQTT IOT Example
- - continuously obtains values from the Virtuabotix DHT11 temperature/humidity sensor
- - formats the results as a JSON string for the IBM IOT example
- - connects to an MQTT server (either local or at the IBM IOT Cloud)
- - and publishes the JSON String to the topic named quickstart:MY_MAC_ADDRESS
- */
+  MQTT IOT Example
+  - continuously obtains values from the Virtuabotix DHT11 temperature/humidity sensor
+  - formats the results as a JSON string for the IBM IOT example
+  - connects to an MQTT server (either local or at the IBM IOT Cloud)
+  - and publishes the JSON String to the topic named quickstart:MY_MAC_ADDRESS
+*/
 
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
-#include <dht11.h>
+
+
+int schalter = 4;
+
+int message = 0;
+
 
 // Update this to either the MAC address found on the sticker on your ethernet shield (newer shields)
 // or a different random hexadecimal value (change at least the last four bytes)
-byte mac[]    = {0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+byte mac[]    = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 char macstr[] = "deedbafefeed";
 // Note this next value is only used if you intend to test against a local MQTT server
-byte localserver[] = {192, 168, 1, 98 };
+// byte localserver[] = {192, 168, 1, 98 };
 // Update this value to an appropriate open IP on your local network
-byte ip[]     = {192, 168, 1, 20 };
+byte ip[]     = {192, 168, 62, 177 };
 
-char servername[]="iot.eclipse.org";
+char servername[] = "iot.eclipse.org";
 String clientName = String("d:quickstart:arduino:") + macstr;
-String topicName = String("iot-2/evt/status/fmt/json");
+//String topicName = String("z1j/home/alarmduino/json");
+String topicName = String("home/zij/alarm");
 
-dht11 DHT11;
-float tempF = 0.0;
-float tempC = 0.0;
-float humidity = 0.0;
 EthernetClient ethClient;
 
 // Uncomment this next line and comment out the line after it to test against a local MQTT server
@@ -36,20 +38,20 @@ PubSubClient client(servername, 1883, 0, ethClient);
 
 void setup()
 {
+  pinMode(schalter, INPUT_PULLUP);
 
   // Start the ethernet client, open up serial port for debugging, and attach the DHT11 sensor
   Ethernet.begin(mac, ip);
   Serial.begin(9600);
-  DHT11.attach(3);
 
 }
 
 void loop()
 {
   char clientStr[34];
-  clientName.toCharArray(clientStr,34);
+  clientName.toCharArray(clientStr, 34);
   char topicStr[26];
-  topicName.toCharArray(topicStr,26);
+  topicName.toCharArray(topicStr, 26);
   getData();
   if (!client.connected()) {
     Serial.print("Trying to connect to: ");
@@ -59,8 +61,8 @@ void loop()
   if (client.connected() ) {
     String json = buildJson();
     char jsonStr[200];
-    json.toCharArray(jsonStr,200);
-    boolean pubresult = client.publish(topicStr,jsonStr);
+    json.toCharArray(jsonStr, 200);
+    boolean pubresult = client.publish(topicStr, jsonStr);
     Serial.print("attempt to send ");
     Serial.println(jsonStr);
     Serial.print("to ");
@@ -70,52 +72,35 @@ void loop()
     else
       Serial.println("unsuccessfully sent");
   }
-  delay(5000);
+  delay(3000);
 }
 
 String buildJson() {
   String data = "{";
-  data+="\n";
-  data+= "\"d\": {";
-  data+="\n";
-  data+="\"myName\": \"Arduino DHT11\",";
-  data+="\n";
-  data+="\"temperature (F)\": ";
-  data+=(int)tempF;
-  data+= ",";
-  data+="\n";
-  data+="\"temperature (C)\": ";
-  data+=(int)tempC;
-  data+= ",";
-  data+="\n";
-  data+="\"humidity\": ";
-  data+=(int)humidity;
-  data+="\n";
-  data+="}";
-  data+="\n";
-  data+="}";
+  data += "\n";
+  data += "\"alarm\": {";
+  data += "\n";
+  data += "\"state\": \"";
+  data += message;
+  data += "\",";
+  data += "\n";
+  data += "}";
+  data += "\n";
+  data += "}";
   return data;
 }
 
 void getData() {
-  int chk = DHT11.read();
-  switch (chk)
+  switch (digitalRead(schalter))
   {
-  case 0: 
-    Serial.println("Read OK"); 
-    humidity = (float)DHT11.humidity;
-    tempF = DHT11.fahrenheit();
-    tempC = DHT11.temperature;
-    break;
-  case -1: 
-    Serial.println("Checksum error"); 
-    break;
-  case -2: 
-    Serial.println("Time out error"); 
-    break;
-  default: 
-    Serial.println("Unknown error"); 
-    break;
+    case LOW:
+      Serial.println("Read OK = 1");
+      message = 1;
+      break;
+    case HIGH:
+      Serial.println("Read OK = 0");
+      message = 0;
+      break;
   }
 }
 
