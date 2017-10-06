ad6c216f760e5df5e233a145f0b606be43cab3a4
zijonas <jonas.ziel@terra.com.br>
2017-10-06 18:27:09 -0300

    Tooltip eingebau

--- sscript/alarmMonitor/status_alarmduino.sh
+++ sscript/alarmMonitor/status_alarmduino.sh
@@ -4,9 +4,14 @@ status=`mosquitto_sub -C 1 -h iot.eclipse.org -p 1883 -t home/zij/alarm | xargs 
 export LASTISALIVE=20
 
 case $status in
-	5)   echo "<img>/opt/alarmMonitor/emergency.png</img>";;
-	10)  echo "<img>/opt/alarmMonitor/orange_shield.png</img>";;
-	30)  echo "<img>/opt/alarmMonitor/green_shield.png</img>";;
-	100) echo "<img>/opt/alarmMonitor/blue_shield.png</img>";;
-	*)   echo "<img>/opt/alarmMonitor/red_shield.png</img>";;
+	5)   echo "<img>/opt/alarmMonitor/emergency.png</img>";
+	     echo "<tool>EMERGENCY!!!!!!</tool>";;
+	10)  echo "<img>/opt/alarmMonitor/orange_shield.png</img>";
+	     echo "<tool>Fired and armed again!</tool>";;
+	30)  echo "<img>/opt/alarmMonitor/green_shield.png</img>";
+	     echo "<tool>Armed and ready!</tool>";;
+	100) echo "<img>/opt/alarmMonitor/blue_shield.png</img>";
+	     echo "<tool>Alarm is disabled</tool>";;
+	*)   echo "<img>/opt/alarmMonitor/red_shield.png</img>";
+	     echo "<tool>Invalid Status or no connection to Host!</tool>";;
 esac
