f0d01980553973bb942327dfbd83bd9b8a54fc9c
zijonas <jonas.ziel@terra.com.br>
2017-10-23 10:08:56 -0200

    Fixed some status Issues

--- sscript/alarmMonitor/status_alarmduino.sh
+++ sscript/alarmMonitor/status_alarmduino.sh
@@ -7,29 +7,33 @@ mosquitto_sub -C 1 -h iot.eclipse.org -p 1883 -t home/zij/alarm > "$TEMP_FILE"
 #status=`mosquitto_sub -C 1 -h iot.eclipse.org -p 1883 -t home/zij/alarm  | grep state | awk '{print $NF}' | cut -d '"' -f2`
 a_stat=`cat "$TEMP_FILE" | grep state | awk '{print $NF}' | cut -d '"' -f2`
 a_time=`cat "$TEMP_FILE" | grep timestamp | awk '{print $NF}' | cut -d '"' -f2`
+stat=$a_stat
 
-
-if [ -f "SERVER_TIME" ]
+if [ -f "$SERVER_TIME" ]
 then
 	p_stime=`cat "$SERVER_TIME"`
-	if [ 	$p_time -eq $a_stime ]
+	if [ $p_stime -eq $a_time ]
 	then
 		p_ltime=`cat "$LOCAL_TIME"`
-		time_diff=$((date +%s - $p_ltime - 5))
+		ttime=`date +%s`
+		time_diff=$(($ttime - $p_ltime - 5))
+#		echo $p_ltime
+#		echo $ttime
+#		echo $time_diff
 		if [ $time_diff -gt $a_stat ]
 		then
-			a_stat=666
+			stat=666
 		fi
 	else
 		echo $a_time > "$SERVER_TIME"
-		echo date +%s > "$LOCAL_TIME"
+		echo `date +%s` > "$LOCAL_TIME"
 	fi
 else
 		echo $a_time > "$SERVER_TIME"
-		echo date +%s > "$LOCAL_TIME"
+		echo `date +%s` > "$LOCAL_TIME"
 fi
 
-case $a_stat in
+case $stat in
 	5)   echo "<img>/opt/alarmMonitor/emergency.png</img>";
 	     echo "<tool>EMERGENCY!!!!!!</tool>";;
 	10)  echo "<img>/opt/alarmMonitor/orange_shield.png</img>";
@@ -39,5 +43,5 @@ case $a_stat in
 	100) echo "<img>/opt/alarmMonitor/blue_shield.png</img>";
 	     echo "<tool>Alarm is disabled</tool>";;
 	*)   echo "<img>/opt/alarmMonitor/red_shield.png</img>";
-	     echo "<tool>Invalid Status or no connection to Host! (Last Status: $stat)</tool>";;
+	     echo "<tool>Invalid Status or no connection to Host! (Last Status: $a_stat)</tool>";;
 esac
