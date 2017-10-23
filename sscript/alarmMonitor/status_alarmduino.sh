33c2934006a8167ddd9eb8afa8f7f48cae97f4fd
zijonas <jonas.ziel@terra.com.br>
2017-10-23 09:09:08 -0200

    Test if has sant messages

--- sscript/alarmMonitor/status_alarmduino.sh
+++ sscript/alarmMonitor/status_alarmduino.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
-status=`mosquitto_sub -C 1 -h iot.eclipse.org -p 1883 -t home/zij/alarm  | grep state | awk '{print $NF}' | cut -d '"' -f2`
+TEMP_FILE="/tmp/.tmp123duino"
+SERVER_TIME="/tmp/.tmp123servertime"
+LOCAL_TIME="/tmp/.tmp123localtime"
 
-time=`mosquitto_sub -C 1 -h iot.eclipse.org -p 1883 -t home/zij/alarm | grep timestamp | awk '{print $NF}' | cut -d '"' -f2`
+mosquitto_sub -C 1 -h iot.eclipse.org -p 1883 -t home/zij/alarm > "$TEMP_FILE"
+#status=`mosquitto_sub -C 1 -h iot.eclipse.org -p 1883 -t home/zij/alarm  | grep state | awk '{print $NF}' | cut -d '"' -f2`
+a_stat=`cat "$TEMP_FILE" | grep state | awk '{print $NF}' | cut -d '"' -f2`
+a_time=`cat "$TEMP_FILE" | grep timestamp | awk '{print $NF}' | cut -d '"' -f2`
 
-delta=1000
 
-last_time=`cat /tmp/lasttimealarmduino`
-echo $time /tmp/lasttimealarmduino
-
-if $time-$lasttime>$status*1000+$delta
-do
-status=666
-done
+if [ -f "SERVER_TIME" ]
+then
+	p_stime=`cat "$SERVER_TIME"`
+	if [ 	$p_time -eq $a_stime ]
+	then
+		p_ltime=`cat "$LOCAL_TIME"`
+		time_diff=$((date +%s - $p_ltime - 5))
+		if [ $time_diff -gt $a_stat ]
+		then
+			a_stat=666
+		fi
+	else
+		echo $a_time > "$SERVER_TIME"
+		echo date +%s > "$LOCAL_TIME"
+	fi
+else
+		echo $a_time > "$SERVER_TIME"
+		echo date +%s > "$LOCAL_TIME"
 fi
 
-case $status in
+case $a_stat in
 	5)   echo "<img>/opt/alarmMonitor/emergency.png</img>";
 	     echo "<tool>EMERGENCY!!!!!!</tool>";;
 	10)  echo "<img>/opt/alarmMonitor/orange_shield.png</img>";
@@ -24,5 +39,5 @@ case $status in
 	100) echo "<img>/opt/alarmMonitor/blue_shield.png</img>";
 	     echo "<tool>Alarm is disabled</tool>";;
 	*)   echo "<img>/opt/alarmMonitor/red_shield.png</img>";
-	     echo "<tool>Invalid Status or no connection to Host!</tool>";;
+	     echo "<tool>Invalid Status or no connection to Host! (Last Status: $stat)</tool>";;
 esac
