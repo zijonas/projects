8a62ec3fbe616c20fb4e41c346afc50596d74a73
zijonas <jonas.ziel@terra.com.br>
2018-06-28 18:01:42 -0300

    Continuing - FIx word count

--- keymaster/keymaster.js
+++ keymaster/keymaster.js
@@ -4,52 +4,54 @@ var correctWord = 0;
 var wrongWords = 0;
 
 function drawChalengeText() {
-	var listChalengeText = document.getElementById("listChalengeText");
-	
-	var inputWord = document.getElementById("inputArea");
-	inputWord.value = "";
+  var listChalengeText = document.getElementById("listChalengeText");
 
-	while (listChalengeText.firstChild && chalengeText.length > 0) {
-		listChalengeText.removeChild(listChalengeText.firstChild);
-	}
+  var inputWord = document.getElementById("inputArea");
+  inputWord.value = "";
 
-	var currIndex;
-	for (currIndex = 0; currIndex < chalengeText.length; currIndex++) {
-		var newLi = document.createElement("li");
-		newLi.setAttribute("id", "word" + currIndex);
-		newLi.innerHTML = chalengeText[currIndex];
-		listChalengeText.appendChild(newLi);
-	}
+  let placar = "Typed = " + typedWords + " | Correct = " + correctWord + " | Wrong = " + wrongWords;
+  document.getElementById('placar').innerHTML = placar;
+
+  while (listChalengeText.firstChild && chalengeText.length > 0) {
+    listChalengeText.removeChild(listChalengeText.firstChild);
+  }
+
+  var currIndex;
+  for (currIndex = 0; currIndex < chalengeText.length; currIndex++) {
+    var newLi = document.createElement("li");
+    newLi.setAttribute("id", "word" + currIndex);
+    newLi.innerHTML = chalengeText[currIndex];
+    listChalengeText.appendChild(newLi);
+  }
 }
 
 function checkCurrWord(event) {
-	var inputWord = document.getElementById("inputArea");
-	var currWord = document.getElementById("word" + typedWords);
-	if (event.which == 32) {
-		if (inputWord.value.trim() == chalengeText[typedWords]) {
-			
-			correctWord++;
-		} else {
-			currWord.style.color = "red";
-			wrongWords++;
-		}
-	var inputWord = document.getElementById("inputArea");
-	inputWord.value = "";
-//		typedWords++;
+  var inputWord = document.getElementById("inputArea");
+  var currWord = document.getElementById("word" + typedWords);
+  if (event.which == 32) {
+    if (inputWord.value.trim() == chalengeText[typedWords]) {
+      correctWord++;
+    } else {
+      currWord.style.color = "red";
+      wrongWords++;
+    }
+    var inputWord = document.getElementById("inputArea");
+    inputWord.value = "";
+    typedWords++;
 
-		chalengeText.splice(0, 1);
-		drawChalengeText();
+    chalengeText.splice(0, 1);
+    drawChalengeText();
 
-	} else {
-		if (inputWord.value == chalengeText[typedWords].substring(0, inputWord.value.length)) {
-			currWord.style.color = "green";
-		} else {
-			currWord.style.color = "red";
-		}
-	}
+  } else {
+    if (inputWord.value == chalengeText[typedWords].substring(0, inputWord.value.length)) {
+      currWord.style.color = "green";
+    } else {
+      currWord.style.color = "red";
+    }
+  }
 }
 
 window.onload = function loadText() {
-	drawChalengeText();
+  drawChalengeText();
 
 };
