c2c72ac72cb1ca85f1bca8473f60b1e764f4d01b
zijonas <jonas.ziel@terra.com.br>
2019-04-23 13:30:43 -0300

    Create rectangel game

---
+++ javascript/rect/rect.js
@@ -0,0 +1,89 @@
+var balls = [];
+window.onload = function() {
+  canvas = document.getElementById('can');
+velx = 10;
+  ctx = canvas.getContext("2d");
+  document.addEventListener("keydown", keyPress);
+  setInterval(game, 1000 / velx);
+  for(i = 0; i < 1; i++)
+    balls[i] = new Ball();
+};
+
+cx = cy = 500;
+mouseVectorx = 1;
+mouseVectory = 0;
+vel = 10;
+apple = new Apple();
+
+function Apple() {
+  this.xPos = Math.trunc(Math.random() * 10) * 50;
+  this.yPos = Math.trunc(Math.random() * 10) * 50;
+  this.xSize = 10;
+  this.ySize = 10;
+
+  this.draw = () => {
+    ctx.fillStyle = "green";
+    ctx.fillRect(this.xPos, this.yPos, this.xSize, this.ySize);
+  }
+}
+
+
+function Ball() {
+  this.xPos = 250;
+  this.yPos = 250;
+  this.xSize = 10;
+  this.ySize = 10;
+
+  this.draw = () => {
+    ctx.fillStyle = "red";
+    ctx.lineTo(this.xPos, this.yPos);
+  }
+}
+
+function game() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.beginPath();
+  balls.forEach((ball, i) => {
+    ball.xPos -= mouseVectorx * vel;
+    ball.yPos -= mouseVectory * vel;
+    balls.forEach((bc, j) => {
+      if(ball.xPos == bc.xPos && ball.yPos == bc.yPos && i != j) {
+        ctx.fill();
+      }
+    });
+    ball.draw();
+  });
+  balls.push(new Ball());
+  ctx.lineWidth = vel;
+  ctx.strokeStyle = 'red';
+  ctx.stroke();
+}
+
+function keyPress(evt) {
+  switch(evt.keyCode) {
+    case 37 :
+      mouseVectorx = -1;
+      mouseVectory = 0;
+      break;
+    case 38 :
+      mouseVectorx = 0;
+      mouseVectory = -1;
+      break;
+    case 39 :
+      mouseVectorx = 1;
+      mouseVectory = 0;
+      break;
+    case 40 :
+      mouseVectorx = 0;
+      mouseVectory = 1;
+      break;
+    case 8 :
+      vel *= 2;
+      break;
+   case 13 :
+      vel /= 2;
+      break;
+  }
+}
