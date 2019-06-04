9537a3cb6ad52860b6aebfda8c55eb674e9934e0
zijonas <jonas.ziel@terra.com.br>
2019-06-03 22:09:33 -0300

    Save task on server

--- frontend/src/app/todo.service.ts
+++ frontend/src/app/todo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { TaskData } from './task-data';
 
 @Injectable({
@@ -12,10 +12,22 @@ export class TodoService {
   constructor(private http: HttpClient) { this.load() }
 
   load() {
-
   }
 
   addItem(task: TaskData, token: string) {
     this.todoList.unshift(task);
+    
+    const getTokenUrl = 'http://localhost:8080/app/insertTask';
+    const getTokenParameters: HttpParams = new HttpParams()
+    .append('task', JSON.stringify(task));
+    const getTokenHeaders: HttpHeaders = new HttpHeaders()
+    .append('Authorization', 'Bearer' + token);;
+
+    return this.http.post(getTokenUrl, {
+        withCredentials: true
+      }, {
+        headers: getTokenHeaders, 
+        params: getTokenParameters
+      }
   }
 }
